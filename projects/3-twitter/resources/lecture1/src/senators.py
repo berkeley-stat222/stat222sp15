acfe9fbf8c30ee698373732095e20030be452119
Jarrod Millman <jarrod.millman@gmail.com>
2015-02-23 02:09:31 -0800

    Final fixes

--- projects/3-twitter/resources/lecture1/src/senators.py
+++ projects/3-twitter/resources/lecture1/src/senators.py
@@ -1,10 +1,10 @@
 import json
 import re
+from operator import itemgetter
 import numpy as np
 import matplotlib.pyplot as plt
 import twitter
 
-# XXX: Go to http://dev.twitter.com/apps/new 
 CONSUMER_KEY       = ""
 CONSUMER_SECRET    = ""
 OAUTH_TOKEN        = ""
@@ -28,14 +28,14 @@ with open("timelines.json", "w") as f:
 # could check who has the most followers
 followers = [t[0]["user"]["followers_count"] for t in timelines]
 zipped = zip(names, followers)
-zipped.sort(key = lambda t: t[1])
+zipped.sort(key=itemgetter(1))
 
 # get all the tweets and see what words are used
 tweets = [" ".join([tweet["text"] for tweet in tweets]) for tweets in timelines]
 words = [w for text in tweets for w in re.split('\W', text) if w]
 vocab = sorted(set(words))
 
-# construct bag-of-words
+# construct document-term matrix
 M = np.asmatrix(np.zeros([len(tweets), len(vocab)]))
 for n, tweet in enumerate(tweets):
     for m, term in enumerate(vocab):
